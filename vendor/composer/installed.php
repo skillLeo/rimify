f2a3dcc6df7544e2d78ebcd6a2a10c247de0d7b9
SkillLeo <hassam.dev.571@gmail.com>
2026-09-23 18:35:47 +0000

    release: ebdc884 fix(release): seed the bore the documents state onto an existing database

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'skillleo/rimify',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '21f635e58dfb4513c6daaf47458c51507e0d4e5c',
+        'reference' => 'ebdc8849dcf780a7a064bd40f3a3172cb414013a',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -828,7 +828,7 @@
         'skillleo/rimify' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '21f635e58dfb4513c6daaf47458c51507e0d4e5c',
+            'reference' => 'ebdc8849dcf780a7a064bd40f3a3172cb414013a',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
